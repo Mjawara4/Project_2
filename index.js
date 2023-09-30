879293ac6c1a1cad7fa9b1254318f58c7b317279
Mjawara4 <101847266+Mjawara4@users.noreply.github.com>
2023-09-29 21:26:52 -0400

    set up  styles in css

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log('working')
-alert('working')
+
+let board;
+
+function init(){
+    board = [
+        '','','',
+        '','','',
+        '','',''
+    ];
+    render();
+};
+
+init();
+
+function render() {
+    board.forEach(functiom(mark, index){
+        console.log(mark, index);
+    });
+};
