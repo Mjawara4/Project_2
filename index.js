b0a1b0d0ff15691b4e0c2ea4431732d86ced5050
Mjawara4 <101847266+Mjawara4@users.noreply.github.com>
2023-09-29 21:48:47 -0400

    set up  js function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// contstant
 
+// apps state (variables)
 let board;
+let turn = 'X';
+
+// function
 
 function init(){
     board = [
@@ -15,5 +20,15 @@ init();
 function render() {
     board.forEach(functiom(mark, index){
         console.log(mark, index);
+    // this sets the text context of the square of the same position to the mark on the board/.    });
+    squares[index].textContent = mark;
     });
 };
+
+// cached element references
+const squares =
+    Array.from(document.querySelectorAll('#board div'));
+
+    // event listener
+
+
