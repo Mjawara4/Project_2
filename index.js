a33964a444ced622eab127c895cacac7342a527e
Mjawara4 <101847266+Mjawara4@users.noreply.github.com>
2023-10-05 08:34:57 -0400

    Final Project

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
-// contstant
+/*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
-// apps state (variables)
+//----- app's state (variables) -----
+
 let board;
 let turn = 'X';
+let win;
 
-// function
+//----- cached element references -----
 
-function init(){
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+//----- event listeners -----
+document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
+document.getElementById('reset-button').addEventListener('click', init);
+
+
+//----- functions -----
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+        });
+        return winner ? winner : board.includes('') ? null : 'T';
+};
+
+function handleTurn() {
+    let idx = squares.findIndex(function(square) {
+        return square === event.target;
+    });
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
+    render();
+};
+
+function init() {
     board = [
-        '','','',
-        '','','',
-        '','',''
+    '', '', '',
+    '', '', '',
+    '', '', ''
     ];
     render();
 };
 
-init();
-
 function render() {
-    board.forEach(functiom(mark, index){
-        console.log(mark, index);
-    // this sets the text context of the square of the same position to the mark on the board/.    });
+    board.forEach(function(mark, index) {
+    //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
-};
-
-// cached element references
-const squares =
-    Array.from(document.querySelectorAll('#board div'));
-
-    // event listener
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    };
 
 
+
+init();
